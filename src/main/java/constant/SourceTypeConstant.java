e1a0b58d1a702e8df0c5a24142bb9ec6eae9ce04
cikiPAD <58922174+cikiPAD@users.noreply.github.com>
2023-12-06 15:57:34 +0800

    Add files via upload

---
+++ src/main/java/constant/SourceTypeConstant.java
@@ -0,0 +1,6 @@
+package io.github.samarium150.mirai.plugin.lolicon.command.constant;
+
+public class SourceTypeConstant {
+    public static final String LOLICON = "lolicon";
+    public static final String NYAN = "nyan";
+}

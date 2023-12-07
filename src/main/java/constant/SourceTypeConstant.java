7de44fdd953c156087161aa93234744673af33e9
cikiPAD <58922174+cikiPAD@users.noreply.github.com>
2023-12-07 15:38:04 +0800

    Delete src/main/java/constant/SourceTypeConstant.java

--- src/main/java/constant/SourceTypeConstant.java
+++
@@ -1,6 +0,0 @@
-package io.github.samarium150.mirai.plugin.lolicon.command.constant;
-
-public class SourceTypeConstant {
-    public static final String LOLICON = "lolicon";
-    public static final String NYAN = "nyan";
-}

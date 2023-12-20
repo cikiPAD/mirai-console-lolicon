04d41c09aa989fff378e39bc712a1d21997d4d3d
cikiPAD <58922174+cikiPAD@users.noreply.github.com>
2023-12-20 11:08:01 +0800

    Update SourceTypeConstant.java

--- src/main/java/constant/SourceTypeConstant.java
+++ src/main/java/constant/SourceTypeConstant.java
@@ -5,9 +5,10 @@ public class SourceTypeConstant {
     public static final String NYAN = "nyan";
     public static final String JITSU = "jitsu";
     public static final String YD = "樱道";
-
     public static final String XLJ = "xlj";
     public static final String VVHAN = "vvhan";
     public static final String TDZ = "tangdouz";
     public static final String STARCHENT = "starchent";
+
+    public static final String ACGMX = "acgmx";
 }

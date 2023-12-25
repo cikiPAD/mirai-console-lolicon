a6fa073c5fdb4a7dccd1ad2fac67d8269d468250
cikiPAD <58922174+cikiPAD@users.noreply.github.com>
2023-12-25 11:04:27 +0800

    Update ImageSourceInterface.java

--- src/main/java/ImageSourceInterface.java
+++ src/main/java/ImageSourceInterface.java
@@ -1,6 +1,7 @@
 package io.github.samarium150.mirai.plugin.lolicon.command;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +11,24 @@ public interface ImageSourceInterface {
     String getType();
 
     List<String> getImageUrl(Map<String, Object> params);
+
+    default List<ImageUrlEntity> getImageUrlEntity(Map<String, Object> params) {
+        List urls = getImageUrl(params);
+        if (urls == null) {
+            return null;
+        }
+
+        if (urls.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        List<ImageUrlEntity> entities = new ArrayList<>();
+        ImageUrlEntity entity = new ImageUrlEntity();
+        entity.setUrls(urls);
+        entity.setDisplayString(String.format("图片来源: %s", getType()));
+        entities.add(entity);
+        return entities;
+    }
 
     Map<String, Object> standardParams(Map<String, Object> params);
 }
